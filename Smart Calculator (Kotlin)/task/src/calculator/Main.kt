eba81b1c98e72479f8b2fb39eed2d9f7cb2253d5
Aaron Howser <aaronhowser1@gmail.com>
2023-04-03 09:17:34 -0500

    forgot to account for 2-digit numbers

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -3,20 +3,40 @@ package calculator
 fun main() {
 
     while (true) {
-        val input = readln().split(" ")
-        if (input.first() == "/exit") break
-        if (input.first() == "/help") {
+        val input = readln()
+        if (input == "/exit") break
+        if (input == "/help") {
             println("The program calculates the sum of numbers")
             continue
         }
-        if (input.first().isBlank()) continue
+        if (input.isBlank()) continue
 
-        var sum = 0
-        for (i in input) sum += i.toInt()
-
-        println(sum)
+        println(calculate(input))
 
     }
     println("Bye!")
 }
 
+fun calculate(input: String): Int {
+
+    var sum = 0
+    var addingNext = true
+
+    for (i in input.indices) {
+
+        val char = input[i]
+        if (char == ' ') continue
+        if (char == '-') {
+            addingNext = !addingNext
+            continue
+        }
+
+        if (char.isDigit()) {
+            if (addingNext) sum += char.digitToInt() else sum -= char.digitToInt()
+            addingNext = true
+        }
+
+    }
+
+    return sum
+}
